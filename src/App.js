668a3d2ca0b87db3a3b5dd8557914aa6738705b3
TruongLe <mrcook.b2@gmail.com>
2019-04-22 21:52:00 +0700

    add antd

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import { Button } from 'antd';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <h1>Hello</h1>
+        <Button type="primary">Primary</Button>
+        <Button>Default</Button>
+        <Button type="dashed">Dashed</Button>
+        <Button type="danger">Danger</Button>
       </div>
     );
   }
